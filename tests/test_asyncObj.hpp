2773006b8d600e9936656a2944eb15758ff97046
DorianGrey <ShadowVampire@web.de>
2013-03-08 20:50:18 +0100

    More test cases: With/without data modification, using lambdas, free functions, and functor struct

--- tests/test_asyncObj.hpp
+++ tests/test_asyncObj.hpp
@@ -10,18 +10,73 @@ namespace conc_test
 {
     namespace async_object
     {
+        struct foo_functor_side
+        {
+            void operator()(std::string& s) const 
+            {
+                s += " Moreover, it's crappy!";
+            }
+        };
+
+        void func_functor_side(std::string& s)
+        {
+            s += " Its not awesome plz.";
+        }
+
+        void test_side_effects()
+        {
+            concurrent::async_object<std::string> blub ("Hello World!");
+            // Testing with lambda expression.
+            auto res = blub <= ( [](std::string& s) -> void{
+                 s += " omfg!";
+            });
+            // Testing with free function.
+            auto res2 = blub <= &func_functor_side; // [Note] std::addressof cannot be used here.
+            // Testing with functor struct.
+            auto res3 = blub <= foo_functor_side();
+
+            blub <= ( [](std::string& s) -> void{
+                std::cout << "<Test result>" << s << std::endl;
+            }); 
+        }
+
+        struct foo_functor
+        {
+            std::string operator()(std::string& s) const 
+            {
+                return s + " Moreover, it's crappy!";
+            }
+        };
+
+        std::string func_functor(std::string& s)
+        {
+            return s + " Its not awesome plz.";
+        }
+
         void test_simple()
         {
             concurrent::async_object<std::string> blub ("Hello World!");
+            // Testing with lambda expression.
             auto res = blub <= ( [](std::string& s) -> std::string{
                 return s + " omfg!";
-            }); 
+            });
+            // Testing with free function.
+            auto res2 = blub <= &func_functor; // [Note] std::addressof cannot be used here.
+            // Testing with functor struct.
+            auto res3 = blub <= foo_functor();
+
             std::cout << res.get() << std::endl;
+            std::cout << res2.get() << std::endl;
+            std::cout << res3.get() << std::endl;
         }
 
         void main()
         {
+            std::cout << "[:: Test 1: Call with no side effects. ::]" << std::endl;
             test_simple();
+
+            std::cout << "[:: Test 2: Call with side effects. ::]" << std::endl;
+            test_side_effects();
         }
     }
 }
--- tests/test_syncObj.hpp
+++ tests/test_syncObj.hpp
@@ -10,17 +10,69 @@ namespace conc_test
 {
     namespace sync_object
     {
-        void test_simple()
+        struct foo_functor_side
+        {
+            void operator()(std::string& s) const 
+            {
+                s += " Moreover, it's crappy!";
+            }
+        };
+
+        void func_functor_side(std::string& s)
+        {
+            s += " Its not awesome plz.";
+        }
+
+        void test_side_effects()
         {
             concurrent::sync_object<std::string> blub("World Hello");
+            // Testing with lambda expression.
             blub <= ( [](std::string& s) -> void {
                 std::cout << s << std::endl;
             });
+            // Testing with free function.
+            blub <= &func_functor_side; // [Note] std::addressof cannot be used here.
+            // testing with functor struct.
+            blub <= foo_functor_side();
+
+            blub <= ( [](std::string& s) -> void{
+                std::cout << "<Test result>" << s << std::endl;
+            }); 
+        }
+
+        struct foo_functor
+        {
+            void operator()(std::string& s) const 
+            {
+                std::cout << s << std::endl;
+            }
+        };
+
+        void func_functor(std::string& s)
+        {
+            std::cout << s << std::endl;
+        }
+
+        void test_simple()
+        {
+            concurrent::sync_object<std::string> blub("World Hello");
+            // Testing with lambda expression.
+            blub <= ( [](std::string& s) -> void {
+                std::cout << s << std::endl;
+            });
+            // Testing with free function.
+            blub <= &func_functor; // [Note] std::addressof cannot be used here.
+            // testing with functor struct.
+            blub <= foo_functor();
         }
 
         void main()
         {
-           test_simple();
+            std::cout << "[:: Test 1: Call with no side effects. ::]" << std::endl;
+            test_simple();
+
+            std::cout << "[:: Test 2: Call with side effects. ::]" << std::endl;
+            test_side_effects();
         }
     }
 }
